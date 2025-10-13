b1983ee7a8915354c53f4687b363dee6a3ff0a13
josk0 <53160902+josk0@users.noreply.github.com>
2025-10-13 11:04:10 -0400

    Update conda_wrapper_eval.sh

--- eval_scripts/conda_wrapper_eval.sh
+++ eval_scripts/conda_wrapper_eval.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-eval "$(/home/$(whoami)/anaconda3/bin/conda shell.bash hook)"
+# eval "$(/home/$(whoami)/anaconda3/bin/conda shell.bash hook)"
+eval "$(/home/dhonchar/anaconda3/bin/conda shell.bash hook)" # use Dan's enviornment to avoid replicating our own
 
 source "/home/$(whoami)/.bashrc"
 export WANDB_PROJECT=eval_model
