dce7df1d02c8cbe3e95fad6f45e3b6bf9560b2d6
josk0 <53160902+josk0@users.noreply.github.com>
2025-10-13 11:27:17 -0400

    debugging conda evns

--- eval_scripts/conda_wrapper_eval.sh
+++ eval_scripts/conda_wrapper_eval.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-# eval "$(/home/$(whoami)/anaconda3/bin/conda shell.bash hook)"
-eval "$(/home/dhonchar/anaconda3/bin/conda shell.bash hook)" # use Dan's enviornment to avoid replicating our own
+eval "$(/home/$(whoami)/anaconda3/bin/conda shell.bash hook)"
 
 source "/home/$(whoami)/.bashrc"
 export WANDB_PROJECT=eval_model
--- llm_scripts/conda_wrapper_fine.sh
+++ llm_scripts/conda_wrapper_fine.sh
@@ -5,7 +5,6 @@ eval "$(/home/dhonchar/anaconda3/bin/conda shell.bash hook)"
 
 source "/home/$(whoami)/.bashrc"
 export WANDB_PROJECT=finetune-llm
-export CUDA_LAUNCH_BLOCKING=1 # for debugging only, for more accurate stacktracing
 
 conda activate llm_finetuning
 
